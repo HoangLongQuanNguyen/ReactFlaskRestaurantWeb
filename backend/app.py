5577ce59ccf145b054f65836d6cca499f02ef54c
HoangLongQuan <hoanglqnguyen@gmail.com>
2025-08-06 15:09:09 -0500

    Configured app.py for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from email.policy import default
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +11,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+# route to get static filr from dist folder in frontend folder
+@app.route("/", defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
+
 import routes
 
 with app.app_context():
