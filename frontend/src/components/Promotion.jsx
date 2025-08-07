0128ab45d938a14028ef634741b1931972ddc5dc
HoangLongQuan <hoanglqnguyen@gmail.com>
2025-08-07 01:33:46 -0500

    Updated Promotion

--- frontend/src/components/Promotion.jsx
+++ frontend/src/components/Promotion.jsx
@@ -3,27 +3,45 @@ import React from 'react'
 
 function Promotion() {
   return (
-    <Container width="dvw"  background="#ffffffff" py={5}>
-        <SimpleGrid minChildWidth="sm" templateColumns={{ sm: '1fr', md: '1fr 1fr' }} spacing={8} rowGap="4">
+    <Container bg="#fff" py={10}>
+        <SimpleGrid
+            columns={{ base: 1, md: 2 }}
+            spacing={6}
+        >
             <Box>
-                <Center>
-                    <Flex direction="column" alignItems={"center"} spaceY={5} py={50}>
-                        <Text fontWeight="bold" textStyle="3xl" color={'yellow.300'}>Welcome</Text>
-                        <Text textStyle="3xl" color={'yellow.300'}>Buy 5 Sandwiches – Get 1 Free!</Text>
-                        <Link href="https://pos.chowbus.com/online-ordering/store/restaurant/15068" textDecoration="none" isExternal>
-                            <Button variant="solid" colorPalette="yellow" fontSize={20} color="white">Order Now</Button>
-                        </Link>
-                    </Flex> 
-                </Center> 
-            </Box> 
-            <Box>
-                <Center>
-                    <img src='/promotion.png' alt='promotion' width={300} height={300} ></img>
-                </Center>   
-            </Box> 
-        </SimpleGrid>  
-    </Container>             
+            <Center>
+                <Flex
+                direction="column"
+                align="center"
+                gap={6}
+                py={10}
+                >
+                <Text fontWeight="bold" fontSize="3xl" color="yellow.300">
+                    Welcome
+                </Text>
+                <Text fontSize="2xl" color="yellow.300" textAlign="center">
+                    Buy 5 Sandwiches – Get 1 Free!
+                </Text>
+                <Link
+                    href="https://pos.chowbus.com/online-ordering/store/restaurant/15068"
+                    isExternal
+                    _hover={{ textDecoration: 'none' }}
+                >
+                    <Button colorPalette="yellow" color="white" variant="solid" fontSize="lg">
+                        Order Now
+                    </Button>
+                </Link>
+                </Flex>
+            </Center>
+            </Box>
 
+            <Box>
+            <Center>
+                <img src="/promotion.png" alt="promotion" width={300} height={300} />
+            </Center>
+            </Box>
+        </SimpleGrid>
+    </Container>
   )
 }
 
