970fa97bf14cab2ecd38a6355b8f375ba7153e4b
HoangLongQuan <hoanglqnguyen@gmail.com>
2025-12-26 12:26:22 -0600

    Added scope to Auth0Provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,8 @@ createRoot(document.getElementById('root')).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-    redirect_uri: window.location.origin,
+      redirect_uri: window.location.origin,
+      scope: "openid profile email",
   }}
     >
       <Provider value={theme}>
